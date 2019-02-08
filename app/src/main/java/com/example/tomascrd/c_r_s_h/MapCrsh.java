5fa6da3cccd79c0a9c86eef6712b7b1a19b9a66c
TomasCardenal <tomas.car.guitv@gmail.com>
2019-02-08 13:58:06 +0100

    Finished main testing on screen resolutions, found a solution for centering the grid on the screen

--- app/src/main/java/com/example/tomascrd/c_r_s_h/MapCrsh.java
+++ app/src/main/java/com/example/tomascrd/c_r_s_h/MapCrsh.java
@@ -12,11 +12,11 @@ import android.graphics.Paint;
 public class MapCrsh {
 
     private int SCREEN_COLUMNS = 32;
-    private int SCREEN_ROWS = 16;
+    private int SCREEN_ROWS = 18;
     private Paint pTiles;
     private MainGameScene gameRef;
     public int mapID;
-
+    int reference, hReference;
     /**
      * Starts a map on this ID and with the indicated reference
      * @param mapID The id for the map to load, -1 for test grid
@@ -25,6 +25,10 @@ public class MapCrsh {
     public MapCrsh(int mapID,MainGameScene gameRef){
         this.gameRef = gameRef;
         this.mapID = mapID;
+        this.pTiles = new Paint();
+        this.pTiles.setColor(Color.GREEN);
+        this.reference = gameRef.screenWidth/SCREEN_COLUMNS;
+        this.hReference = (gameRef.screenHeight-reference*SCREEN_ROWS)/2;
     }
 
     /**
@@ -33,15 +37,12 @@ public class MapCrsh {
      */
     public void draw(Canvas c){
         if(this.mapID==-1){
-            pTiles = new Paint();
-            int reference = gameRef.screenWidth/SCREEN_COLUMNS;
             boolean redFirst = true;
-            pTiles.setColor(Color.GREEN);
-            //Grid test (IT WORKS, on my phone at least)
+            //Grid test (IT WORKS, ON EVERY PHONE) Adjust settings to start drawing wherever we want to
             for(int i=1;i<SCREEN_ROWS-1;i++){
                 for(int j=2;j<SCREEN_COLUMNS-2;j++){
                     pTiles.setColor(pTiles.getColor()==Color.GREEN?Color.RED:Color.GREEN);
-                    c.drawRect(j*reference,i*reference,(j+1)*reference,(i+1)*reference,pTiles);
+                    c.drawRect(j*reference,i*reference+hReference,(j+1)*reference,(i+1)*reference+hReference,pTiles);
                 }
                 if(redFirst){
                     pTiles.setColor(Color.RED);
